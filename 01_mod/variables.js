b5c63d85110ac4ff8a37792a46503f948625af9c
Hiten Sharma <hitens384@gmail.com>
2025-12-31 11:13:08 +0000

    container config done

---
+++ 01_mod/variables.js
@@ -0,0 +1,9 @@
+const accountId=144523
+let accountEmail="hiten54@gmail.com"
+var accountPassword="919254"
+accountCity="Jaipur"
+accountEmail="hc@hc.com"
+accountPassword="919291"
+accountCity="Jalandhar"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
