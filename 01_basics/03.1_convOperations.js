a8cc2920f7c267fa25908d7fd1c2e81da61580b9
Hiten Sharma <hitens384@gmail.com>
2026-01-02 16:21:35 +0000

    container config done

---
+++ 01_basics/03.1_convOperations.js
@@ -0,0 +1,24 @@
+let value=3
+let negvalue=-value
+console.log(negvalue)
+
+
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(6/2)
+console.log(4%8)*/
+
+
+let str1="Hello"
+let str2=" Hiten"
+str3= str1 + str2
+console.log(str3)
+
+
+console.log(1+"2")
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+"2"+2)
---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,14 @@
+let value=54
+
+console.log(typeof value)
+
+let isLoggedIn="Hiten"
+
+let BooleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
+
+// If we convert 1 into boolean output is true
+//Similarly if we convert 0 into boolean output is false
+
+// If we convert "" into boolean output is true
+//if "Value" output is false
