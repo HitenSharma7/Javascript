df6f1459d60b6ed79bf4c20715ec76734fb7f388
Hiten Sharma <hitens384@gmail.com>
2026-01-07 13:52:27 +0000

    container config done

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+//console.log(2>3)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//In JavaScript ,Comparison operators(<,>,<=,>=) and Equality signs work differently
+//Comparisons convert null into a number(i.e :0) that is why null>=0 is a true
+
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<0)
