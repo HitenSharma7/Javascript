592c9ed0d5f34f8550ff5b73eaa283f0288d1855
Hiten Sharma <hitens384@gmail.com>
2026-01-07 18:25:40 +0000

    container config done

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -11,5 +11,5 @@ console.log(null>=0)
 // console.log(undefined==0)
 // console.log(undefined>0)
 // console.log(undefined<0)
-
+ 
 console.log("2"===2)
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,31 @@
+// Primitive Datatypes : String,Number,Boolean,Null,Symbol,Undefined,BigInt
+
+
+
+const score =100
+const isLoggedIn =false
+const outsideTemp=null
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId)
+
+
+
+// Non Primitive Datatypes(Reference): Array,Objects,Functions
+
+const GOATS=["Arthur Morgan","Niko Bellic","Tommy Vercciti","Carl Johnson","John Martson"]
+
+console.log(GOATS)
+
+let myObj={
+    Name:"Hiten",
+    Age:20,
+}
+console.log(myObj)
+
+const myFunction=function()
+{
+console.log("My name is John Martson deu deu deu ")
+}
