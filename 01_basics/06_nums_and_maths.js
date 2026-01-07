6720e2d69d34a745657450e6a704cfb91dc8c9db
Hiten Sharma <hitens384@gmail.com>
2026-01-07 19:52:40 +0000

    container config done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+const score=400
+console.log(score)
+
+const balance=new Number(100)
+console.log(balance.toString.length) //here  by using toString we can use functionalities of a String
+
+console.log(balance.toFixed(2))
+
+const otherNumber= new Number(123.8956)
+
+console.log(otherNumber.toPrecision(5)) //roundoff karta hai
+
+
+
+//++++++++++++++++++++Maths+++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-5))
+console.log(Math.round(5.3535))
+console.log(Math.max(21,12,33,54,85,69,58,49,47))
+console.log(Math.random(1,2,3,4,5,6))
+console.log(Math.floor(5.3))
+console.log(Math.ceil(9.1))
