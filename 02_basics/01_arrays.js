a945696928dc639331848b0ed01b48f85f451150
Hiten Sharma <hitens384@gmail.com>
2026-01-09 18:06:06 +0000

    container config done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,18 @@
 //array
 
 const myArr=[0,1,2,3,4,5,6]
+// const GOATs=["Vishu Don","Rehmaan Dakait"]
+
+
 //JavaScript Arrays are basically resizable.
 //We can add  elements of different datatypes in the array
+
+const myArr2= new Array(1,2,3,4)
+console.log(myArr[1])
+
+myArr.push(7)
+myArr.push(8)
+myArr.push(9)
+myArr.pop(7)
+
+console.log(myArr)
