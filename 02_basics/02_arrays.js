d4e1c626efedb55aeb88356936f348d33e6bdd3f
Hiten Sharma <hitens384@gmail.com>
2026-01-14 18:15:56 +0000

    container config done

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -28,7 +28,7 @@ console.log(Array.from("Hiten Sharma"))
 console.log(Array.from({name:"Hiten Sharma"}))  //Interesting..... Always Empty Array
 let score1=100
 let score2=200
-let score3=300
+let score3=350
 
 console.log(Array.of(score1,score2,score3))
 
