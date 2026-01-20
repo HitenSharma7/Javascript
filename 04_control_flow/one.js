ff9dfef49a83c2a83996a20de6ea660adb12161a
Hiten Sharma <hitens384@gmail.com>
2026-01-20 16:23:11 +0000

    container config done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+//if
+const isUserLoggedin= true
+const temperature=-56
+if(temperature>30)
+{
+    console.log("It is Summer Seson")
+}
+else{
+    console.log("It is winter season")
+}
+
+//var has global scope
+//let and const have local scope and it is better
