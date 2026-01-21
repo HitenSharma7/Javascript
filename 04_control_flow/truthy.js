9946ecad667879c7875d8a38a070ae6122792fd6
Hiten Sharma <hitens384@gmail.com>
2026-01-21 15:35:00 +0000

    container config done

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -14,3 +14,21 @@ else{
 
 //truthy values
 //"0",'false'," ",[],{},function(){}
+const EmptyObject={}
+
+if(Object.keys(EmptyObject).length==0)
+{
+ console.log("The Object is empty")
+}
+//Nullish Coalscing Operator(??):null undefined
+let val1;
+// val1=5??10
+// val1= null??10
+// val1=undefined??15
+val1=null??10??15
+console.log(val1)
+
+//Terniary Operator
+//condition true:false
+const iceTeaPrice=100
+iceTeaPrice <=80? console.log("less than 80"):console.log("more than 80")
