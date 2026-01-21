374c398a3c10a2e595f7e47cc75d8ed2a082a89e
Hiten Sharma <hitens384@gmail.com>
2026-01-21 14:32:41 +0000

    container config done

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,16 @@
+const UserEmail=[]
+
+if (UserEmail){
+    console.log("Got User Email")
+}
+
+else{
+    console.log("Did not get email")
+}
+
+ // These are some falsy values,Rest all are truthy
+// 0,Nan,null,undefined,"",-0,false,BigInt 0n
+
+
+//truthy values
+//"0",'false'," ",[],{},function(){}
