1eef863e69a2f3bb321416d1a6ad655d41ef900d
Hiten Sharma <hitens384@gmail.com>
2026-01-25 14:32:35 +0000

    container conifg done

---
+++ 05_iterations/six.js
@@ -0,0 +1,10 @@
+const coding=["JS","C++","Ruby","C","Python","Java"]
+
+const values=coding.forEach((item)=>{ 
+     console.log(item)
+     return item   //forEach do not return any thing
+}
+)
+
+console.log(values)
+const Mynums=[1,2,3,4,5,6,7,8,9,10]
