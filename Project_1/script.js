b1ae418fa037f0f4f1185e94abd4a473cfc9764e
Hiten Sharma <hitens384@gmail.com>
2026-02-06 12:21:57 +0000

    container config done

--- Project_1/script.js
+++ Project_1/script.js
@@ -1,2 +1,49 @@
-const buttons=document.querySelector('.button')
-const body = document.querySelector('body')
+const buttons = document.querySelectorAll(".button");
+const body=document.querySelector("body")
+
+buttons.forEach(function(button){
+  button.addEventListener("click", function(e){
+   console.log(e)
+   console.log(e.target)
+
+   if(e.target.id==='grey'){
+    body.style.backgroundColor=e.target.id;
+    
+   }
+
+   if(e.target.id==='red'){
+    body.style.backgroundColor=e.target.id;
+    
+   }
+
+   if(e.target.id==='blue'){
+    body.style.backgroundColor=e.target.id;
+    
+   }
+
+   if(e.target.id==='yellow'){
+    body.style.backgroundColor=e.target.id;
+    
+   }
+   if(e.target.id==='purple'){
+    body.style.backgroundColor=e.target.id;
+    
+   }
+   if(e.target.id==='black'){
+    body.style.backgroundColor=e.target.id;
+    
+   }
+   if(e.target.id==='beige'){
+    body.style.backgroundColor='beige';
+    
+   }
+   if(e.target.id==='orange'){
+    body.style.backgroundColor=e.target.id;
+    
+   }
+   if(e.target.id==='crimson'){
+    body.style.backgroundColor=e.target.id;
+    
+   }
+  });
+});
