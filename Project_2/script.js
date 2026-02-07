22be1b0c57bffda6bcd8d6ecaf1bb7a309a4e34b
Hiten Sharma <hitens384@gmail.com>
2026-02-07 14:15:03 +0000

    container config done

--- Project_2/script.js
+++ Project_2/script.js
@@ -5,6 +5,32 @@ form.addEventListener('submit',function (e){
 
   const height=parseInt(document.querySelector('#height').value)
   const weight=parseInt(document.querySelector('#weight').value)
-  //if we write this particular value outside this event... Then at the time when the page loads,
+  //if we write this particular value outside this event... Then at the time(instance) when the page loads,it stores the default empty value.Because it is before the event
   
+   const results=document.querySelector('#results')
+
+   if(height<0||height===""||isNaN(height)){
+    results.innerHTML="Please give the valid height."
+   }
+   else if(weight<0||weight===""||isNaN(weight)){
+    results.innerHTML="Please give the valid weight."
+   }
+   else{
+   const bmi= (weight / ((height*height)/10000)).toFixed(2)
+   results.innerHTML=`<span>${bmi}</span>`
+   
+   if(bmi>24.9){
+    
+    results.innerHTML="You are Overweight"
+    console.log(`${bmi}`)
+   }
+   else if(bmi<18.6){
+    results.innerHTML="Underweight"
+    console.log(`${bmi}`)
+   }
+   else{
+    results.innerHTML="You are fit soldier,Keep Hitting"
+     console.log(`${bmi}`)
+   }
+   }
 })
