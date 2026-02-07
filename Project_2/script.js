2536a9b7f7598e267d3fc705030b6b5036b089cd
Hiten Sharma <hitens384@gmail.com>
2026-02-07 13:43:33 +0000

    container config done

---
+++ Project_2/script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit',function (e){
+   e.preventDefault()
+
+  const height=parseInt(document.querySelector('#height').value)
+  const weight=parseInt(document.querySelector('#weight').value)
+  //if we write this particular value outside this event... Then at the time when the page loads,
+  
+})
